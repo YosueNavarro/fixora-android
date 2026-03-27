5bcc73146fd14b7b91df9dc3cf0bbd5767ff9e8e
yosueclase <yosnavmol@alu.edu.gva.es>
2026-03-27 19:08:55 +0100

    commitp rovisional, arreglo de bugs

--- app/src/main/java/com/tynsolutions/gestionaveriasmovil/domain/model/Maquinaria.kt
+++ app/src/main/java/com/tynsolutions/gestionaveriasmovil/domain/model/Maquinaria.kt
@@ -1,11 +1,10 @@
 package com.tynsolutions.gestionaveriasmovil.domain.model
 
 /**
- * Entidad de dominio que representa un activo físico del taller.
+ * Entidad de dominio que representa un activo físico (hardware) del taller.
  */
 data class Maquinaria(
     val id: Int,
     val nombre: String,
-    val codigoEstado: Int = 802
+    val codigoEstado: Int = 0
 )
-
